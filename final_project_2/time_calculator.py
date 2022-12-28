7d2128f6a98b0a54e0aad783506f7f5dd3b990d0
Andres felipe Castro salazar <fepipe22@gmail.com>
2022-12-28 16:52:43 +0100

    black tool pep8

--- final_project_2/time_calculator.py
+++ final_project_2/time_calculator.py
@@ -7,7 +7,6 @@ def add_time(time, sum, day=None):
     sum_hour = int(sum_hour)
     sum_minutes = int(sum_minutes)
 
-
     final_hour = hour + sum_hour
     final_minute = minutes + sum_minutes
 
@@ -21,18 +20,17 @@ def add_time(time, sum, day=None):
         final_hour_1 = final_hour - (days * 24)
         if final_hour_1 > 12:
             final_hour_1 -= 12
-        
 
     if 12 < final_hour < 24 and final_minute > 0:
         final_hour_1 = final_hour - 12
-    
+
     if final_hour <= 12:
         final_hour_1 = final_hour
-    
+
     if am_pm == "AM":
         select_am_pm = final_hour // 24
     elif am_pm == "PM":
-        select_am_pm = (final_hour // 12)
+        select_am_pm = final_hour // 12
         if final_hour % 24 != 0:
             select_am_pm += 1
 
@@ -40,18 +38,18 @@ def add_time(time, sum, day=None):
         if final_hour >= 24:
             hour_pm = [even for even in range(100) if even % 2 == 0]
             hour_pm = [prime for prime in range(100) if prime % 2 != 0]
-            
+
             if select_am_pm in hour_am:
                 am_pm_output = "AM"
             elif select_am_pm in hour_pm:
                 am_pm_output = "PM"
-        
+
         elif final_hour < 12:
             am_pm_output = "AM"
-        
+
         else:
             am_pm_output = "PM"
-        
+
     elif am_pm == "PM":
         if final_hour >= 36 and final_minute > 0:
             # print(f'--{final_hour}--')
@@ -67,22 +65,21 @@ def add_time(time, sum, day=None):
 
         elif final_hour < 12:
             am_pm_output = "PM"
-        
+
         elif 12 <= final_hour < 24:
             am_pm_output = "AM"
-        
+
         elif 24 <= final_hour < 36 and final_minute == 0:
             am_pm_output = "PM"
-        
 
     result = f"{final_hour_1}:{final_minute:02d} {am_pm_output}"
 
     if am_pm == "AM":
-            days = final_hour // 24
+        days = final_hour // 24
     elif am_pm == "PM":
-            days = (final_hour / 24) + 1
+        days = (final_hour / 24) + 1
 
-    if not day: 
+    if not day:
         if am_pm == "AM":
             if days < 1:
                 return result
@@ -135,7 +132,8 @@ def add_time(time, sum, day=None):
 
             if final_hour >= 36:
                 return f"{result}, {final_day} ({int(days)} days later)"
-        
+
+
 if __name__ == "__main__":
 
     print(add_time("3:00 PM", "3:10"))
